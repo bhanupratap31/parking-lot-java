d62a444a094acee4ae43bb724eba2f2274d5c414
bhanupratap31 <bhanudtg@gmail.com>
2026-07-14 22:56:49 +0530

    folder structure and invalid package error fixed

---
+++ parkinglot/vehicle/Bike.java
@@ -0,0 +1,8 @@
+package parkinglot.vehicle;
+
+public class Bike extends Vehicle {
+
+    public Bike(String licenseNumber) {
+        super(licenseNumber, VehicleSize.SMALL);
+    }
+}
---
+++ parkinglot/vehicle/Car.java
@@ -0,0 +1,8 @@
+package parkinglot.vehicle;
+
+public class Car extends Vehicle {
+
+    public Car(String licenseNumber) {
+        super(licenseNumber, VehicleSize.MEDIUM);
+    }
+}
